3981f84f612b6080c2c2e220cf31f66ce08c4dd6
JEDSDOFFICIAL <166973866+JEDSDOFFICIAL@users.noreply.github.com>
2025-06-25 03:35:00 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ import Head from "next/head";
 export const metadata: Metadata = {
   title: "JEDSD | Journal of Embedded and Digital System Design ",
   description: "JEDSD is a peer-reviewed journal that publishes high-quality research in the field of embedded and digital system design. The journal aims to provide a platform for researchers, engineers, and practitioners to share their findings and advancements in this rapidly evolving field.",
-
+ icons: {
+    icon: "/logored.jpg",
+  },
 };
 
 export default function RootLayout({
