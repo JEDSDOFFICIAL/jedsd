5216a451afc93ff7909eced01a151053472e7d3f
suman Mishra <suman132j@gmail.com>
2025-06-25 03:55:41 +0530

    favicon set properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   title: "JEDSD | Journal of Embedded and Digital System Design ",
   description: "JEDSD is a peer-reviewed journal that publishes high-quality research in the field of embedded and digital system design. The journal aims to provide a platform for researchers, engineers, and practitioners to share their findings and advancements in this rapidly evolving field.",
 icons: {
-    icon: "/fevicon.ico",
+    icon: "/favicon.ico",
   },
 };
 
